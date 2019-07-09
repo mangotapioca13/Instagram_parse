c40d8c9b13447bacc2afe119b99058912a9673df
Angela <angela.liu1399@gmail.com>
2019-07-09 12:17:47 -0700

    create Post class

--- app/src/main/java/com/example/instagram/HomeActivity.java
+++ app/src/main/java/com/example/instagram/HomeActivity.java
@@ -3,10 +3,17 @@ package com.example.instagram;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.instagram.model.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -17,6 +24,34 @@ public class HomeActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        loadTopPosts();
+    }
+
+    private void createPost(String description, ParseFile imageFile, ParseUser user) {
+        // TODO -- create and save post
+    }
+
+    public void loadTopPosts() {
+        // create new Post.Query
+        final Post.Query postsQuery = new Post.Query();
+        postsQuery.getTop().withUser();
+
+        // grabs all posts in the background thread
+        postsQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < objects.size(); i++) {
+                        Log.d("HomeActivity", "Post[" + i + "] = "
+                                + objects.get(i).getDescription()
+                                + "\nusername = " + objects.get(i).getUser().getUsername());
+                    }
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     // methods to handle click on the LogOut menu item
--- app/src/main/java/com/example/instagram/ParseApp.java
+++ app/src/main/java/com/example/instagram/ParseApp.java
@@ -2,12 +2,17 @@ package com.example.instagram;
 
 import android.app.Application;
 
+import com.example.instagram.model.Post;
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // register a subclass so parse knows that this is a custom parse model
+        ParseObject.registerSubclass(Post.class);
 
         final Parse.Configuration configuration = new Parse.Configuration.Builder(this)
                 .applicationId("parsetagram")
