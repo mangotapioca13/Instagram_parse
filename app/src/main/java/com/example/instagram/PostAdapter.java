3b925fddaffcb2f5f465bf2259434672857a8639
Angela <angela.liu1399@gmail.com>
2019-07-10 23:16:04 -0700

    timestamp configuration and password editText completed

--- app/src/main/java/com/example/instagram/PostAdapter.java
+++ app/src/main/java/com/example/instagram/PostAdapter.java
@@ -70,6 +70,7 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
         public void bind(Post post) {
             tvUserName.setText(post.getUser().getUsername());
             tvDescription.setText(post.getDescription());
+            tvTimeAgo.setText(post.getTimeAgo());
 
             ParseFile image = post.getImage();
             if (image != null) {
--- app/src/main/java/com/example/instagram/model/Post.java
+++ app/src/main/java/com/example/instagram/model/Post.java
@@ -1,9 +1,15 @@
 package com.example.instagram.model;
 
+import android.text.format.DateUtils;
+
 import com.parse.ParseClassName;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 @ParseClassName("Post")
 public class Post extends ParseObject {
@@ -26,6 +32,11 @@ public class Post extends ParseObject {
         return getParseUser(KEY_USER);
     }
 
+    public String getTimeAgo() {
+        String time = getCreatedAt().toString();
+        return getRelativeTimeAgo(time);
+    }
+
     // setter methods
     public void setDescription(String description) {
         put(KEY_DESCRIPTION, description);
@@ -37,5 +48,24 @@ public class Post extends ParseObject {
 
     public void setUser(ParseUser user) {
         put(KEY_USER, user);
+    }
+
+    // createdAt format: Wed Jul 10 22:19:31 PDT 2019
+    public static String getRelativeTimeAgo(String time) {
+        String instagramFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(instagramFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+
+        try {
+            long dateMillis = sf.parse(time).getTime();
+            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return relativeDate.toLowerCase();
     }
 }
